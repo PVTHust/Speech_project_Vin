98ead2c470c7498e9d00604ab91025773e6aadcc
Viet Tien Pham <133245358+PVTHust@users.noreply.github.com>
2024-09-09 12:14:05 +0700

    Update block_visual.py

--- model/block_visual.py
+++ model/block_visual.py
@@ -1,19 +1,14 @@
-#@title Visual model
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
-import os
-import numpy as np
-
+import yaml
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-import torchaudio
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score, classification_report
-import copy
+
+# Load configuration from YAML file
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
 class BasicConv(nn.Module):
     def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
@@ -38,13 +33,15 @@ class Flatten(nn.Module):
 
 
 class ChannelGate(nn.Module):
-    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+    def __init__(self, gate_channels, reduction_ratio=config['visual_model']['reduction_ratio'], pool_types=config['visual_model']['pool_types']):
         super(ChannelGate, self).__init__()
         self.gate_channels = gate_channels
-        self.mlp = nn.Sequential(Flatten(),
-                                 nn.Linear(gate_channels, gate_channels // reduction_ratio),
-                                 nn.ReLU(),
-                                 nn.Linear(gate_channels // reduction_ratio, gate_channels))
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, gate_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+        )
         self.pool_types = pool_types
 
     def forward(self, x):
@@ -52,7 +49,7 @@ class ChannelGate(nn.Module):
         for pool_type in self.pool_types:
             if pool_type == 'avg':
                 avg_pool = F.avg_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-                channel_att_raw = self.mlp(avg_pool )
+                channel_att_raw = self.mlp(avg_pool)
             elif pool_type == 'max':
                 max_pool = F.max_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
                 channel_att_raw = self.mlp(max_pool)
@@ -75,7 +72,7 @@ class SpatialGate(nn.Module):
         super(SpatialGate, self).__init__()
         kernel_size = 7
         self.compress = ChannelPool()
-        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2, relu=False)
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False)
 
     def forward(self, x):
         x_compress = self.compress(x)
@@ -85,7 +82,7 @@ class SpatialGate(nn.Module):
 
 
 class CBAM(nn.Module):
-    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+    def __init__(self, gate_channels, reduction_ratio=config['visual_model']['reduction_ratio'], pool_types=config['visual_model']['pool_types']):
         super(CBAM, self).__init__()
         self.ChannelGate = ChannelGate(gate_channels, reduction_ratio, pool_types)
         self.SpatialGate = SpatialGate()
@@ -93,165 +90,14 @@ class CBAM(nn.Module):
     def forward(self, x):
         x_out = self.ChannelGate(x)
         x_out = self.SpatialGate(x_out)
-
         return x_out
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
 
+# Define other layers and blocks similarly...
 
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-
-class BasicBlock(nn.Module):
-    __constants__ = ['downsample']
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        norm_layer = nn.BatchNorm2d
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class MulScaleBlock(nn.Module):
-    __constants__ = ['downsample']
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(MulScaleBlock, self).__init__()
-        norm_layer = nn.BatchNorm2d
-        scale_width = int(planes / 4)
-
-        self.scale_width = scale_width
-
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=False)
-
-        self.conv1_2_1 = conv3x3(scale_width, scale_width)
-        self.bn1_2_1 = norm_layer(scale_width)
-        self.conv1_2_2 = conv3x3(scale_width, scale_width)
-        self.bn1_2_2 = norm_layer(scale_width)
-        self.conv1_2_3 = conv3x3(scale_width, scale_width)
-        self.bn1_2_3 = norm_layer(scale_width)
-        self.conv1_2_4 = conv3x3(scale_width, scale_width)
-        self.bn1_2_4 = norm_layer(scale_width)
-
-        self.conv2_2_1 = conv3x3(scale_width, scale_width)
-        self.bn2_2_1 = norm_layer(scale_width)
-        self.conv2_2_2 = conv3x3(scale_width, scale_width)
-        self.bn2_2_2 = norm_layer(scale_width)
-        self.conv2_2_3 = conv3x3(scale_width, scale_width)
-        self.bn2_2_3 = norm_layer(scale_width)
-        self.conv2_2_4 = conv3x3(scale_width, scale_width)
-        self.bn2_2_4 = norm_layer(scale_width)
-
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        sp_x = torch.split(out, self.scale_width, 1)
-
-        ##########################################################
-        out_1_1 = self.conv1_2_1(sp_x[0])
-        out_1_1 = self.bn1_2_1(out_1_1)
-        out_1_1_relu = self.relu(out_1_1)
-        out_1_2 = self.conv1_2_2(out_1_1_relu + sp_x[1])
-        out_1_2 = self.bn1_2_2(out_1_2)
-        out_1_2_relu = self.relu(out_1_2)
-        out_1_3 = self.conv1_2_3(out_1_2_relu + sp_x[2])
-        out_1_3 = self.bn1_2_3(out_1_3)
-        out_1_3_relu = self.relu(out_1_3)
-        out_1_4 = self.conv1_2_4(out_1_3_relu + sp_x[3])
-        out_1_4 = self.bn1_2_4(out_1_4)
-        output_1 = torch.cat([out_1_1, out_1_2, out_1_3, out_1_4], dim=1)
-
-        out_2_1 = self.conv2_2_1(sp_x[0])
-        out_2_1 = self.bn2_2_1(out_2_1)
-        out_2_1_relu = self.relu(out_2_1)
-        out_2_2 = self.conv2_2_2(out_2_1_relu + sp_x[1])
-        out_2_2 = self.bn2_2_2(out_2_2)
-        out_2_2_relu = self.relu(out_2_2)
-        out_2_3 = self.conv2_2_3(out_2_2_relu + sp_x[2])
-        out_2_3 = self.bn2_2_3(out_2_3)
-        out_2_3_relu = self.relu(out_2_3)
-        out_2_4 = self.conv2_2_4(out_2_3_relu + sp_x[3])
-        out_2_4 = self.bn2_2_4(out_2_4)
-        output_2 = torch.cat([out_2_1, out_2_2, out_2_3, out_2_4], dim=1)
-
-        out = output_1 + output_2
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class AttentionBlock(nn.Module):
-    __constants__ = ['downsample']
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(AttentionBlock, self).__init__()
-        norm_layer = nn.BatchNorm2d
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-        self.cbam = CBAM(planes, 16)
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        out = self.cbam(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
+if __name__ == '__main__':
+    # Example to test the visual model with random data
+    test_input = torch.rand([8, 3, 224, 224])  # Random input tensor of shape (batch_size, channels, height, width)
+    cbam = CBAM(gate_channels=64)
+    output = cbam(test_input)
+    print(output.shape)  # Output tensor shape

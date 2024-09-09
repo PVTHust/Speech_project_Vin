62d1de1e59d99a12e2ba9a29c52441a8b2defa8a
Viet Tien Pham <133245358+PVTHust@users.noreply.github.com>
2024-09-09 12:04:03 +0700

    Update block_audio.py

--- model/block_audio.py
+++ model/block_audio.py
@@ -1,19 +1,26 @@
-
 import torch
 import torch.nn as nn
 from torch.cuda.amp import autocast
 import os
 import wget
 import timm
-from timm.models.layers import to_2tuple,trunc_normal_
+from timm.models.layers import to_2tuple, trunc_normal_
+import yaml
 
-# override the timm package to relax the input shape constraint.
+# Load the configuration from the YAML file
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# Override the timm package to relax the input shape constraint.
 class PatchEmbed(nn.Module):
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+    def __init__(self, config):
         super().__init__()
-
-        img_size = to_2tuple(img_size)
-        patch_size = to_2tuple(patch_size)
+        
+        img_size = to_2tuple(config['img_size'])
+        patch_size = to_2tuple(config['patch_size'])
+        in_chans = config['in_chans']
+        embed_dim = config['embed_dim']
+        
         num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
         self.img_size = img_size
         self.patch_size = patch_size
